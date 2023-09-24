2a0c4e5cd12837c13f4eb525e0ac2942e8bb0494
Yevhen Oliinyk <oleynik.zh10@gmail.com>
2023-09-24 20:10:58 +0300

    Logs.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { interval } from 'rxjs';
 
 async function bootstrap() {
+  interval(3000).subscribe(() => {
+    console.log("New Log!")
+  })
+
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
 }
