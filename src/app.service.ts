87e8e0caec7a3c24fa22f61c543583338cf5deec
Zhenya Oleynik <46522647+rangoku@users.noreply.github.com>
2023-09-27 21:24:57 +0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'NEW !!! Hello World! x!!!222222!';
+    return 'New release!';
   }
 }
