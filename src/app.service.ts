7f804abdca247b996543c8a0d81dfcef65198ea6
Yevhen Oliinyk <oleynik.zh10@gmail.com>
2023-11-18 21:16:21 +0200

    redis.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { createClient } from 'redis';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'New release!';
+    return 'New World!';
+  }
+
+  async redis() {
+
+    if (!process.env.REDIS_CLUSTER_IP) {
+      return "NO REDIS_CLUSTER_IP"
+    }
+
+    const client = await createClient({ password: 'admin', url: `redis://${process.env.REDIS_CLUSTER_IP}` })
+      .on('error', err => console.log('Redis Client Error', err))
+      .connect();
+
+    await client.set('key', 'TEST_REDIS');
+    const value = await client.get('key');
+    await client.disconnect();
+
+    return value
   }
 }
